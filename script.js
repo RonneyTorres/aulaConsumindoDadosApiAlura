dfbbff526f125dfdebf64e4498792c2052ef5b8d
ronneypris26@gmail.com <ronneypris26@gmail.com>
2023-04-27 17:48:15 -0300

    manupulação de DOM

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 async function buscaEndeco(cep) {
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let consultaCEPConvertida = await consultaCEP.json();
         if(consultaCEPConvertida.erro) {
             throw Error('CEP inexistente!');
         }
+        let cidade = document.getElementById('cidade');
+        let logradouro = document.getElementById('endereco');
+        let estado = document.getElementById('estado');
+        let bairro = document.getElementById('bairro')
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch(erro) {
+        mensagemErro.innerHTML = `<p>Cep inválido. Tente novamente!</p>`
         console.log(erro) 
     }
 }
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndeco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndeco(cep.value));
